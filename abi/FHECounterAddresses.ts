fa0aaae986e4cff0fccdd9b44668518d88e1a10a
flowersonthefloor <flowersonthefloor@proton.me>
2025-10-27 18:45:09 +0000

    Add FHECounterAddresses.ts

---
+++ abi/FHECounterAddresses.ts
@@ -0,0 +1,8 @@
+
+/*
+  This file is auto-generated.
+  Command: 'npm run genabi'
+*/
+export const FHECounterAddresses = { 
+  "11155111": { address: "0x821668De8ee3E9132D54CeF2eed7528eC2Dc9167", chainId: 11155111, chainName: "sepolia" },
+};
